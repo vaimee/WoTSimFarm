eeabd64c3ae5ff8116ef181a01a422e30e117a68
reluc <relu.cri@gmail.com>
2020-09-05 09:17:21 +0200

    feat: evaporation

--- src/simulation.ts
+++ src/simulation.ts
@@ -5,9 +5,9 @@ export default class Simulation {
     private simObjects: Map<Sprinkler,Array<SoilSensor>>;
     private timeOfTheDay: number;
 
-    constructor(sunRiseHour:number,sunSetHour:number) {
+    constructor() {
         this.simObjects = new Map();
-        this.timeOfTheDay = 0;
+        this.timeOfTheDay = 0; // 60 = 1 hour
     }
 
     add(sprinkler:Sprinkler,sensors:Array<SoilSensor> = []){
@@ -15,20 +15,33 @@ export default class Simulation {
     }
 
     step(){
+        this.timeOfTheDay++
+        let dayHour = Math.floor(this.timeOfTheDay / 60)
+        
+        if (dayHour > 23){
+            dayHour = 0;
+            this.timeOfTheDay = 0
+        }
+
         for (let [sprinkler, sensors] of this.simObjects) {
             sensors.forEach(sensor => {
                 if (sprinkler.isActive){
                     sensor.moisture += 1
                 }
 
-                const timeOfTheDayBooster = 1
-                // Evaporation
-                sensor.moisture -= 0.2 * timeOfTheDayBooster;
+                sensor.moisture = this.evaporate(sensor.moisture,dayHour);
             })
            
         }
-       
 
+    }
 
+    private evaporate(moisture:number,dayHour:number) {
+        // 2/(e^(x/2)+e^(-x/2)) min x = -6 and x = 6 max x = 0
+        let x = dayHour - 12 //max solar evaporation at noon
+        const e = Math.exp
+        const timeOfTheDayBooster = 2 / (e(dayHour / 2) - e(-dayHour / 2))
+        // Evaporation
+        return moisture - 0.2 * timeOfTheDayBooster;
     }
 }
