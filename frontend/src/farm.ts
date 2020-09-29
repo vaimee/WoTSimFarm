9126d925b566cb8627d86b27bcb640f10b892ad6
reluc <relu.cri@gmail.com>
2020-09-29 10:23:44 +0200

    feat(frontend): improve lighting and camera

--- frontend/src/farm.ts
+++ frontend/src/farm.ts
@@ -1,8 +1,9 @@
 
-import { Engine, Scene, ArcRotateCamera, HemisphericLight, Vector3, Mesh, MeshBuilder, SceneLoader } from 'babylonjs';
+import { Engine, Scene, ArcRotateCamera, HemisphericLight, Vector3, Mesh, MeshBuilder, SceneLoader, ParticleHelper, ParticleSystem, Texture, ConeParticleEmitter, PointParticleEmitter, GPUParticleSystem, TransformNode } from 'babylonjs';
 import {OBJFileLoader} from 'babylonjs-loaders';
 import loadSimulation from "./simulation";
 import SkyMaterial from './materials/skymaterial';
+import { lightFragment } from 'babylonjs/Shaders/ShadersInclude/lightFragment';
 
 
 
@@ -22,12 +23,14 @@ function createScene(): Scene {
         const engine = await loadSimulation(scene)
         const box = MeshBuilder.CreateBox("skyBox", {size:1000 }, scene);
         box.position.y -= 100;
-        
+        scene.ambientColor = new BABYLON.Color3(0.3, 0.3, .3);
         const sky = new SkyMaterial(scene);
         box.material = sky;
 
         const cam = scene.cameras[0] as ArcRotateCamera
         cam.wheelDeltaPercentage = 0.01;
+        cam.upperRadiusLimit = 200;
+        cam.checkCollisions = true;
         scene.useRightHandedSystem = true;
         setInterval(()=> engine.run(),1000)
     })
@@ -35,6 +38,7 @@ function createScene(): Scene {
     return scene;
 }
 
+window.addEventListener("resize", function () { engine.resize(); });
 var scene: Scene = createScene();
 
 engine.runRenderLoop(() => {
