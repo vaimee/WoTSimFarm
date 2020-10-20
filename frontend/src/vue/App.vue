f68355f53355bac8118a5007c438f3519292d45a
Gianluca <gianluca.aguzzi.studio@gmail.com>
2020-10-20 09:25:53 +0200

    add support to single file component, start to create the main page layout

---
+++ frontend/src/vue/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app>
+        <splash-screen/>
+        <v-main>
+          <main-pane/>
+        </v-main>
+    </v-app>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  import SplashScreen from './components/SplashScreen.vue';
+  import MainPane from './view/MainPane.vue';
+  @Component({
+    components: { SplashScreen, MainPane }
+  })
+  export default class extends Vue { }
+</script>
