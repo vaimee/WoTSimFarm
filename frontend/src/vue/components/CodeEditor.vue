51c7871b66e007662e3cd76368c931648c79d404
reluc <relu.cri@gmail.com>
2020-10-23 18:29:29 +0200

    feat(frontend):sync editor code to vuex

--- frontend/src/vue/components/CodeEditor.vue
+++ frontend/src/vue/components/CodeEditor.vue
@@ -3,17 +3,22 @@
 </template>
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import store from '../plugins/vuex';
 import * as monaco from 'monaco-editor';
 @Component
 export default class CodeEditor extends Vue {
     mounted() {
-        monaco.editor.create(document.getElementById("editor")!, {
+        const editor = monaco.editor.create(document.getElementById("editor")!, {
 	        value: '//WoT Code! Enjoy !!!!',
             language: 'typescript',
             theme: "vs",
             renderWhitespace: 'all',
             automaticLayout: true
         });
+
+        editor.onDidChangeModelContent( (e:any) => {
+            store.commit("updateCode",editor.getValue());
+        })
     }
 } 
 </script>
