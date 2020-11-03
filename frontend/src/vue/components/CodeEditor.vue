4dc0e7392ecde327460e93b7ebdc49c47a220f61
reluc <relu.cri@gmail.com>
2020-11-03 09:30:46 +0100

    feat(frontend):wot code completition

--- frontend/src/vue/components/CodeEditor.vue
+++ frontend/src/vue/components/CodeEditor.vue
@@ -4,9 +4,18 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import store from '../plugins/vuex';
+//import nodeWoTDef from '!!raw-loader!../../monaco/node-wot'
 import * as monaco from 'monaco-editor';
 @Component
 export default class CodeEditor extends Vue {
+    created() {
+        const nodeWoTLib =  require('!!raw-loader!../../monaco/node-wot.d.ts').default
+        const WoTLib = require('!!raw-loader!../../monaco/wot.d.ts').default
+        monaco.languages.typescript.javascriptDefaults.addExtraLib(nodeWoTLib, 'ts:filename/node-wot.d.ts');
+        monaco.languages.typescript.javascriptDefaults.addExtraLib(WoTLib, 'ts:filename/wot.d.ts');
+        monaco.editor.createModel(nodeWoTLib, 'typescript', monaco.Uri.parse('ts:filename/node-wot.d.ts'));
+        monaco.editor.createModel(WoTLib, 'typescript', monaco.Uri.parse('ts:filename/wot.d.ts'));
+    }
     mounted() {
         const editor = monaco.editor.create(document.getElementById("editor")!, {
 	        value: store.state.code,
